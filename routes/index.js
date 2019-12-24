4dcc55ae149757e45a8f47fdd20210486687ae20
Alanna Tai <alanna.tai@hotmail.com>
2019-12-23 20:06:46 -0500

    Add google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+))
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/students');
+})
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Grail',
+    user: req.user,
+    name: req.query.name
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,25 +3,35 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 require('dotenv').config();
 
 const app = express();
 
 require('./config/database');
+require('./config/passport');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
