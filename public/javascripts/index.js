ab4733b59de57f5634fb39d1e604cc366d5a60db
Alanna Tai <alanna.tai@hotmail.com>
2020-01-10 04:08:26 -0500

    Small style update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
 	$('.cta-open').on('click', function() {
 		$('.toggle-form, .formwrap').addClass('active');
 		$('.icon-close').addClass('open');
